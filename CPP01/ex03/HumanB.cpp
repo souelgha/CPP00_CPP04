59f3c8548d53bf8e6a580cc4790fee3e436264b9
Sonia Ouelghabet <sonouelg@made-f0Br7s7.clusters.42paris.fr>
2024-09-03 14:27:13 +0200

    update ex03

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: sonouelg <sonouelg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:28:31 by sonouelg          #+#    #+#             */
-/*   Updated: 2024/09/02 16:47:13 by sonouelg         ###   ########.fr       */
+/*   Updated: 2024/09/03 14:17:00 by sonouelg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"HumanB.hpp"
 
-HumanB::HumanB(std::string nom):name(nom){}
+HumanB::HumanB(std::string nom):type(NULL), name(nom){}
 HumanB::~HumanB(){}
 
 void HumanB::setWeapon(Weapon& arm)
@@ -21,5 +21,8 @@ void HumanB::setWeapon(Weapon& arm)
 }
 void HumanB::attack() const
 {
-		std::cout << name << " attacks with their " << type->getType() << std::endl;
+		if(type)
+			std::cout << name << " attacks with their " << type->getType() << std::endl;
+		else
+			std::cout << name << " doesn 't  have weapon" << std::endl;
 }
--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sonouelg <sonouelg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:38:13 by sonouelg          #+#    #+#             */
-/*   Updated: 2024/09/02 16:45:21 by sonouelg         ###   ########.fr       */
+/*   Updated: 2024/09/03 14:24:52 by sonouelg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,19 @@ int main()
 		club.setType("some other type club");
 		bob.attack();
 	}
- {
+	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type club");
+		jim.attack();
+	 }
+	{
+		
+		HumanB jim("Jim");
+		jim.attack();
+		Weapon club = Weapon("crude spiked club");
 		jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type club");
